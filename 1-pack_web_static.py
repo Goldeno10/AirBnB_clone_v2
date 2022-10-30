e105cb9cca87da2853c9edebf5256dc17c2229ce
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-10-31 00:52:11 +0100

    debugging fab script archiver

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-""" Contains a function that compress a folder """
-from datetime import datetime
-from fabric.api import local
+"""
+Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack.
+"""
+
+
 import os
+from datetime import datetime
+from fabric.api import *
 
 
 def do_pack():
-    """ Compresses a folder """
+    """ Compresses a folder and contents to a tgz archive """
     try:
         if not os.path.exists("versions"):
             local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_path = f"versions/web_static_{t.strftime(f)}.tgz"
-        local(f'tar -cvzf {archive_path} web_static/')
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(date))
+        local('tar -cvzf {} web_static/'.format(archive_path))
         return archive_path
     except Exception:
         return None
