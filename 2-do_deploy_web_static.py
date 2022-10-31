f15d4460f29ec6b3ea6c61e9427a8fe41d2a0931
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-10-31 03:25:01 +0100

    Full deployment implementation

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ web_static folder of your AirBnB Clone repo, using the function do_pack.
 
 import os
 from datetime import datetime
-from fabric.api import env, local, put, run
+from fabric.api import env, local, put, sudo
 
 
 env.hosts = ['34.204.198.163', '54.85.106.26']
@@ -40,14 +40,14 @@ def do_deploy(archive_path):
         arch_n_x = arch_f_n.split(".")[0]
 
         path = "/data/web_static/releases/"
-        put(archive_path, '/temp/', use_sudo=True)
-        run("sudo mkdir -p {}{}/".format(path, arch_n_x))
-        run("sudo tar -xzf /temp/{} -C {}{}".format(arch_path, path, arch_n_x))
-        run("sudo rm  /temp/{}".format(archive_f_n))
-        run("sudo mv {0}{1}/web_static/* {0}{1}/".format(path, arch_n_x))
-        run("sudo rm -rf {0}{1}/web_static".format(path, arch_n_x))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {}{}/ /data/web_static/current".format(path, arch_n_x))
+        put(archive_path, "/temp/", use_sudo=True)
+        sudo("mkdir -p {}{}/".format(path, arch_n_x))
+        sudo("tar -xzf /temp/{} -C {}{}".format(arch_path, path, arch_n_x))
+        sudo("rm  /temp/{}".format(archive_f_n))
+        sudo("mv {0}{1}/web_static/* {0}{1}/".format(path, arch_n_x))
+        sudo("rm -rf {0}{1}/web_static".format(path, arch_n_x))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {}{}/ /data/web_static/current".format(path, arch_n_x))
         return True
     except Exception:
         return False
