72eb1ea87445ae95e6f21e7c72b8d501d93cdb52
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-10-31 03:59:55 +0100

    Full deployment implementation

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -41,13 +41,13 @@ def do_deploy(archive_path):
 
         path = "/data/web_static/releases/"
         put(archive_path, "/tmp/", use_sudo=True)
-        run("mkdir -p {}{}/".format(path, arch_n_x))
-        run("tar -xzf /tmp/{} -C {}{}".format(arch_path, path, arch_n_x))
-        run("rm  /tmp/{}".format(archive_f_n))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(path, arch_n_x))
-        run("rm -rf {0}{1}/web_static".format(path, arch_n_x))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(path, arch_n_x))
+        run("sudo mkdir -p {}{}/".format(path, arch_n_x))
+        run("sudo tar -xzf /tmp/{} -C {}{}".format(arch_path, path, arch_n_x))
+        run("sudo rm  /tmp/{}".format(archive_f_n))
+        run("sudo mv {0}{1}/web_static/* {0}{1}/".format(path, arch_n_x))
+        run("sudo rm -rf {0}{1}/web_static".format(path, arch_n_x))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}{}/ /data/web_static/current".format(path, arch_n_x))
         print("New version deployed!")
         return True
     except Exception:
