bb3ef59193292ae56f5adc9fd9e8e692ac4032a1
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-11-13 23:25:48 +0000

    almost done fixing project

--- models/base_model.py
+++ models/base_model.py
@@ -28,11 +28,11 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            for key in kwargs.keys():
+                if key in ["updated_at", "created_at"]:
+                    kwargs[key] = datetime.strptime(kwargs[key], '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "__class__":
+                    del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
