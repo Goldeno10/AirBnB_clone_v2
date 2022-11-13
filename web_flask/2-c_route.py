d6d68b73fc10bcde73523eecb0ef6825fa86e6ff
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-11-13 16:35:25 +0100

    fixed pycodestyle recommendation failures

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hdnd():
 @app.route("/c/<text>", strict_slashes=False)
 def c_is_fun(text):
     """display “C ”, followed by the value of the text variable"""
-    return f"C {text.replace('_', ' ')}"
+    return "C " + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
